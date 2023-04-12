1433d9abe88c88c8bf0d124182141e95b2cdd5e6
Rachana <miyapuramrachana@gmail.com>
2023-04-12 22:33:15 +0530

    working code for all cart controller

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -239,161 +239,93 @@ const getCartForUser = async (req, res) => {
   }
 };
 // order self-pickup
-// const ChangeToSelfPickup = async (req, res) => {
-//   try {
-//     const userId = req.data._id;
-
-//     // find user
-//     const user = await User.findById(userId);
-//     if (!user) {
-//       return res.status(400).json({
-//         status: false,
-//         message: "User not found",
-//       });
-//     }
-
-//     // check if there is an existing cart with status inCart
-//     let existingCart = user.pendingCart.find((c) => c.status === "inCart");
-//     const { transactionId, cookingInstructions, ReceivedAmount } =
-//       req.body;
-//     if (existingCart) {
-//       // update the existing cart status to "Self-pickup"
-//       existingCart.status = "Self-pickup";
-
-//       // remove the existing cart from the pendingCart array
-//       user.pendingCart.splice(user.pendingCart.indexOf(existingCart), 1);
-
-//       // Recalculate the total amount for the cart
-//       existingCart.totalAmount = existingCart.products.reduce(
-//         (total, p) => total + p.price * p.quantity,
-//         0
-//       );
-//       existingCart.DeliveryCharge = 0;
-//       existingCart.GovtTaxes = 20;
-//       existingCart.GrandTotal =
-//         Number(existingCart.totalAmount) +
-//         Number(existingCart.DeliveryCharge) +
-//         Number(existingCart.GovtTaxes);
-//       existingCart.transactionId = transactionId;
-//       existingCart.cookingInstructions = cookingInstructions;
-//       existingCart.ReceivedAmount = ReceivedAmount;
-
-//       // Convert cart object to plain object to avoid issues with Mongoose
-//       const cartToSave = { ...existingCart.toObject(), ...req.body, status: existingCart.status };
-//       delete cartToSave._id;
-
-//       // Add cart to selfPickupCart array
-//       user.selfPickupCart.push({
-//         ...cartToSave,
-//         createdAt: new Date(),
-//       });
-
-//       // Save the changes to the database
-//       await Promise.all([
-//         user.save(),
-//         Cart.findOneAndUpdate({ _id: existingCart._id }, existingCart, {
-//           upsert: true,
-//         }),
-//       ]);
-
-//       return res.status(200).json({
-//         status: true,
-//         message: "Cart updated successfully",
-//         response: [existingCart],
-//       });
-//     } else {
-//       return res.status(404).json({
-//         status: false,
-//         message: "Cart not found",
-//         response: [],
-//       });
-//     }
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({
-//       status: false,
-//       message: "Internal Server Error",
-//       response: error.message,
-//     });
-//   }
-// };
 const ChangeToSelfPickup = async (req, res) => {
   try {
-    // Retrieve userId from JWT token
-    const userId = req.data.id;
+    const userId = req.data._id;
 
-    // Find pending cart for user
+    // find user
     const user = await User.findById(userId);
-    const pendingCartIndex = user.pendingCart.findIndex(
-      (p) => p.status === "inCart"
-    );
-    if (pendingCartIndex === -1) {
-      pendingCartIndex.createdAt = new Date();
-      return res
-        .status(404)
-        .json({ status: false, message: "Pending cart not found" });
+    if (!user) {
+      return res.status(400).json({
+        status: false,
+        message: "User not found",
+      });
     }
 
-    const { transactionId, status, cookingInstructions, ReceivedAmount } =
+    // check if there is an existing cart with status inCart
+    let existingCart = user.pendingCart.find((c) => c.status === "inCart");
+    const { transactionId, cookingInstructions, ReceivedAmount } =
       req.body;
+    if (existingCart) {
+      // update the existing cart status to "Self-pickup"
+      existingCart.status = "Self-pickup";
 
-    if (status === "Self-pickup") {
-      // Move pending cart data to completed cart and empty pending cart
-      const selfPickupCart = user.selfPickupCart || [];
-      const cartToMove = user.pendingCart[pendingCartIndex];
+      // move the existing cart to the selfPickupCart array
+      user.selfPickupCart.push(existingCart);
+
+      // remove the existing cart from the pendingCart array
+      user.pendingCart.splice(user.pendingCart.indexOf(existingCart), 1);
 
       // Recalculate the total amount for the cart
-      cartToMove.totalAmount = cartToMove.products.reduce(
+      existingCart.totalAmount = existingCart.products.reduce(
         (total, p) => total + p.price * p.quantity,
         0
       );
-      cartToMove.DeliveryCharge = 0;
-      cartToMove.GovtTaxes = 20;
-      cartToMove.GrandTotal =
-        Number(cartToMove.totalAmount) +
-        Number(cartToMove.DeliveryCharge) +
-        Number(cartToMove.GovtTaxes);
+      existingCart.DeliveryCharge = 0;
+      existingCart.GovtTaxes = 20;
+      existingCart.GrandTotal =
+        Number(existingCart.totalAmount) +
+        Number(existingCart.DeliveryCharge) +
+        Number(existingCart.GovtTaxes);
+      existingCart.transactionId = transactionId;
+      existingCart.cookingInstructions = cookingInstructions;
+      existingCart.ReceivedAmount = ReceivedAmount;
+      
+      // Convert cart object to plain object to avoid issues with Mongoose
+      const cartToSave = existingCart.toObject();
+      delete cartToSave._id;
 
-      cartToMove.status = status;
-      cartToMove.transactionId = transactionId;
-      cartToMove.cookingInstructions = cookingInstructions;
-      cartToMove.ReceivedAmount = ReceivedAmount;
-
-      selfPickupCart.push({
-        ...cartToMove.toObject(),
+      // Add cart to completed cart array
+      user.completedCart.push({
+        ...cartToSave,
         transactionId,
-        status,
         cookingInstructions,
         ReceivedAmount,
+        createdAt: new Date(),
       });
 
-      user.selfPickupCart = selfPickupCart;
-      user.pendingCart.splice(pendingCartIndex, 1);
+      // Save the changes to the database
+      await Promise.all([
+        user.save(),
+        Cart.findOneAndUpdate(
+          { _id: existingCart._id },
+          existingCart,
+          { upsert: true }
+        ),
+      ]);
 
-      // Save the user object to the database
-      await user.save();
-
-      res.status(200).json({
+      return res.status(200).json({
         status: true,
         message: "Cart updated successfully",
-        response: selfPickupCart,
+        response: [existingCart],
       });
     } else {
-      res.status(400).json({ status: false, message: "Invalid status" });
+      return res.status(404).json({
+        status: false,
+        message: "Cart not found",
+        response: [],
+      });
     }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
       status: false,
-      message: "Internal server error",
-      response: err.message,
+      message: "Internal Server Error",
+      response: error.message,
     });
   }
 };
-
-
-
-// update cart
+// update
 const updateCartStatus = async (req, res) => {
   try {
     // Retrieve userId from JWT token
