6c4efc3e3942528c6aefa3dc4b5b2b531f653043
vadlurirajkumar <vadlurirajkumar6@gmail.com>
2023-05-18 12:24:41 +0530

    reverted back routes

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -23,7 +23,7 @@ cartRoute.get("/getRecentOrder/non-veg", isOtpAuth, getRecentOrderNonVegProducts
 cartRoute.post("/cancelLastOrder", isOtpAuth, cancelLastOrder)
 
 //removeQuantity for product
-cartRoute.post("/removeFromCart", isOtpAuth, removeFromCart)
+cartRoute.patch("/removeFromCart", isOtpAuth, removeFromCart)
 
 //update cart
 cartRoute.patch("/cartUpdate", isOtpAuth, updateCartStatus)
--- routes/privacypolicyRoute.js
+++ routes/privacypolicyRoute.js
@@ -6,7 +6,7 @@ const isAdminAuth = require("../middleware/adminAuth")
 // routes
 privacyPolicyRoute.get("/get-policy" , getPrivacy) // get privacy-policy
 privacyPolicyRoute.post("/add", isAdminAuth, addPrivacy)  // adding privacy policy
-privacyPolicyRoute.put("/update", isAdminAuth, updatePrivacy)  // updating privacy policy
+privacyPolicyRoute.patch("/update", isAdminAuth, updatePrivacy)  // updating privacy policy
 privacyPolicyRoute.delete("/delete/:id", isAdminAuth, deletePrivacy)  // deleting privacy policy
 
 module.exports = privacyPolicyRoute
